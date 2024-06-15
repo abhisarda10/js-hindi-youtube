e6130382609194d295bb3f07a688a05d128d7b32
abhishek sarda <63191393+abhisarda10@users.noreply.github.com>
2024-06-15 13:28:54 +0000

    1

---
+++ 01_basic/01_varibles.js
@@ -0,0 +1,20 @@
+const accountId = 15466
+let accountEmail = "abhisheksarda777@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 15466  const can not be re-defined
+accountEmail = "yt@yt.com"
+accountPassword = "354384"
+accountCity = "ahjd"
+
+/*
+dont use var
+*/`
+
+console.log(accountId);
+console.log(accountPassword);
+console.log(accountEmail);
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
